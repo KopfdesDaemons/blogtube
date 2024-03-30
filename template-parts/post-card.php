648b6531c54129cd6d33bfc9f403d405cb9dc705
Rico <mailvonrico@gmail.com>
2024-03-30 18:46:25 +0100

    author name and image in the feed

--- template-parts/post-card.php
+++ template-parts/post-card.php
@@ -2,8 +2,26 @@
 function blogtube_display_post_card()
 {
     ob_start(); // Start output buffering
+
+    $blogtube_author_id = get_the_author_meta('ID');
+    $blogtube_author_avatar = get_avatar($blogtube_author_id, 32);
+
+    // Get the current date and time
+    $blogtube_current_time = current_time('timestamp');
+
+    // Get the post date
+    $blogtube_post_time = get_the_time('U');
+
+    // Calculate the difference between the current time and the post time
+    $blogtube_time_difference = human_time_diff($blogtube_post_time, $blogtube_current_time);
 ?>
     <div class="blogtube_post_card">
+        <div class="blogtube_post_card_author_row">
+            <a href="<?php echo esc_url(get_author_posts_url($blogtube_author_id)); ?>">
+                <?php echo $blogtube_author_avatar; ?>
+            </a>
+            <?php the_author_link() ?>
+        </div>
         <?php if (has_post_thumbnail() && get_theme_mod('feed_post_card_image', true)) { ?>
             <a class="blogtube_post_card_image_container" href="<?php the_permalink(); ?>">
                 <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
@@ -13,7 +31,8 @@ function blogtube_display_post_card()
             <a href="<?php the_permalink(); ?>">
                 <h2><?php the_title(); ?></h2>
             </a>
-            <span class="blogtube_post_card_image_date"><?php the_date(); ?></span>
+            <?php the_author_link() ?>
+            <span class="blogtube_post_card_date"><?php printf(__('%s ago', 'blogtube'), $blogtube_time_difference);?></span>
             <?php the_excerpt(); ?>
             <!-- <div class="blogtube_post_card_tags_div">
                 <?php
